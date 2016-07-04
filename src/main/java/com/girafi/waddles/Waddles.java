8d24fe0d4a205cc96a5fa554404010d7e2a82481
GirafiStudios <girafi0@gmail.com>
2016-07-04 13:04:00 +0200

    Ported to 1.10.2 Works on all MC versions from 1.9.4 to 1.10.2 Added BoP support

--- src/main/java/com/girafi/waddles/Waddles.java
+++ src/main/java/com/girafi/waddles/Waddles.java
@@ -1,5 +1,7 @@
 package com.girafi.waddles;
 
+import biomesoplenty.api.biome.BOPBiomes;
+import biomesoplenty.core.BiomesOPlenty;
 import com.girafi.waddles.entity.EntityPenguin;
 import com.girafi.waddles.proxy.CommonProxy;
 import com.girafi.waddles.utils.ConfigurationHandler;
@@ -7,8 +9,10 @@ import com.girafi.waddles.utils.Reference;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.init.Biomes;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.storage.loot.LootTableList;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -18,7 +22,7 @@ import net.minecraftforge.fml.common.registry.EntityRegistry;
 
 import java.io.File;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, dependencies = Reference.DEPENDENCIES, guiFactory = Reference.GUI_FACTORY_CLASS)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, acceptedMinecraftVersions="[1.9.4,1.11)", dependencies = Reference.DEPENDENCIES, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class Waddles {
     @Mod.Instance(Reference.MOD_ID)
     public static Waddles instance;
@@ -38,10 +42,22 @@ public class Waddles {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         EntityRegistry.registerModEntity(EntityPenguin.class, "penguin", 0, Waddles.instance, 64, 1, true, 0x000000, 0xFFFFFF);
-        EntityRegistry.addSpawn(EntityPenguin.class, 1, 2, 5, EnumCreatureType.CREATURE, Biomes.ICE_PLAINS, Biomes.MUTATED_ICE_FLATS, Biomes.FROZEN_OCEAN, Biomes.COLD_BEACH);
+        addPenguinSpawn(Biomes.ICE_PLAINS, Biomes.MUTATED_ICE_FLATS, Biomes.FROZEN_OCEAN, Biomes.COLD_BEACH);
+        if (Loader.isModLoaded(BiomesOPlenty.MOD_ID)) {
+            if (BOPBiomes.glacier.isPresent()) {
+                addPenguinSpawn(BOPBiomes.glacier.get());
+            }
+            if (BOPBiomes.gravel_beach.isPresent()) {
+                addPenguinSpawn(BOPBiomes.gravel_beach.get());
+            }
+        }
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
+    }
+
+    private void addPenguinSpawn(Biome... biomes) {
+        EntityRegistry.addSpawn(EntityPenguin.class, 2, 2, 5, EnumCreatureType.CREATURE, biomes);
     }
 }
--- src/main/java/com/girafi/waddles/utils/Reference.java
+++ src/main/java/com/girafi/waddles/utils/Reference.java
@@ -7,5 +7,5 @@ public class Reference {
     public static final String CLIENT_PROXY_CLASS = "com.girafi.waddles.proxy.ClientProxy";
     public static final String COMMON_PROXY_ClASS = "com.girafi.waddles.proxy.CommonProxy";
     public static final String GUI_FACTORY_CLASS = "com.girafi.waddles.client.gui.GuiFactory";
-    public static final String DEPENDENCIES = "required-after:Forge@[12.17.0,)";
+    public static final String DEPENDENCIES = "required-after:Forge@[12.17.0.1976,)";
 }
