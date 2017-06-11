85b2eeee6fa8997be6e8587745fcf413567d6f46
GirafiStudios <girafi0@gmail.com>
2017-06-11 15:37:36 +0200

    Updated to 1.12

--- src/main/java/com/girafi/waddles/entity/EntityAdeliePenguin.java
+++ src/main/java/com/girafi/waddles/entity/EntityAdeliePenguin.java
@@ -14,6 +14,7 @@ import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemFishFood.FishType;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.world.World;
@@ -60,7 +61,7 @@ public class EntityAdeliePenguin extends EntityAnimal {
     }
 
     @Override
-    protected SoundEvent getHurtSound() {
+    protected SoundEvent getHurtSound(DamageSource source) {
         return WaddlesSounds.ADELIE_HURT;
     }
 
