d75ec38bdf8cbf0bb3a0ed6a7ee75c4a2d1b16df
GirafiStudios <girafi0@gmail.com>
2017-06-28 22:33:43 +0200

    Fixed penguins spawning in biomes with an empty spawnable list Changed spawn rates from between 2 and 5, to between 1 and 4

--- src/main/java/com/girafi/waddles/Waddles.java
+++ src/main/java/com/girafi/waddles/Waddles.java
@@ -48,11 +48,11 @@ public class Waddles {
         List<Biome> spawnable_biomes = Lists.newArrayList();
         for (Biome biome : Biome.REGISTRY) {
             Set<BiomeDictionary.Type> types = Sets.newHashSet(BiomeDictionary.getTypesForBiome(biome));
-            if (types.contains(SNOWY) && !types.contains(FOREST) && !types.contains(NETHER)) {
+            if (types.contains(SNOWY) && !types.contains(FOREST) && !types.contains(NETHER) && !biome.getSpawnableList(EnumCreatureType.CREATURE).isEmpty()) {
                 System.out.println("Spawn Penguin");
                 spawnable_biomes.add(biome);
             }
         }
-        EntityRegistry.addSpawn(EntityAdeliePenguin.class, 2, 2, 5, EnumCreatureType.CREATURE, spawnable_biomes.toArray(new Biome[spawnable_biomes.size()]));
+        EntityRegistry.addSpawn(EntityAdeliePenguin.class, 2, 1, 4, EnumCreatureType.CREATURE, spawnable_biomes.toArray(new Biome[spawnable_biomes.size()]));
     }
 }
