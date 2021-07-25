b1201aea79c582853ea11d2417cbc76873e188d9
GirafiStudios <faj10@msn.com>
2021-07-25 17:32:02 +0200

    Moved to using new EntityRenderersEvents, to work with latest version of Forge

--- src/main/java/com/girafi/waddles/client/ClientHandler.java
+++ src/main/java/com/girafi/waddles/client/ClientHandler.java
@@ -5,16 +5,27 @@ import com.girafi.waddles.client.model.PenguinModel;
 import com.girafi.waddles.client.renderer.PenguinRenderer;
 import com.girafi.waddles.init.PenguinRegistry;
 import net.minecraft.client.model.geom.ModelLayerLocation;
-import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.fmlclient.registry.RenderingRegistry;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod.EventBusSubscriber(modid = Waddles.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ClientHandler {
     public static ModelLayerLocation PENGUIN_LAYER = new ModelLayerLocation(new ResourceLocation(Waddles.MOD_ID, "penguin"), "penguin");
 
     public static void init() {
-        //RenderingRegistry.registerEntityRenderingHandler(PenguinRegistry.ADELIE_PENGUIN.get(), PenguinRenderer::new);
-        EntityRenderers.register(PenguinRegistry.ADELIE_PENGUIN.get(), PenguinRenderer::new); //TODO Temporary workaround, as the Forge way is broken
-        RenderingRegistry.registerLayerDefinition(PENGUIN_LAYER, PenguinModel::createBodyLayer);
+
+    }
+
+    @SubscribeEvent
+    public static void registerEntityRenders(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(PenguinRegistry.ADELIE_PENGUIN.get(), PenguinRenderer::new);
+    }
+
+    @SubscribeEvent
+    public static void registerLayerDefinition(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(PENGUIN_LAYER, PenguinModel::createBodyLayer);
     }
 }
